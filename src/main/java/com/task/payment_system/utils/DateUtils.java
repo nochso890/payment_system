b66fc58c6e8fa9de206f532c897ea18aeb4d9b1a
nochso <nochso890@gmail.com>
2024-02-05 21:57:12 +0900

    - DateUtl 수정.

--- src/main/java/com/task/payment_system/utils/DateUtils.java
+++ src/main/java/com/task/payment_system/utils/DateUtils.java
@@ -1,25 +1,44 @@
 package com.task.payment_system.utils;
 
+import com.task.payment_system.payment.enums.PaymentError;
+import com.task.payment_system.payment.exception.PaymentException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
 import java.util.Calendar;
 import java.util.Date;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateUtils {
+    public static boolean isCardExpiryDated(String cardExpiryDate) throws ParseException{
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
+        var split = getStrExpireYear(cardExpiryDate).split("/");
+        int expiryYear = Integer.parseInt(split[1]);
+        int expiryMonth = Integer.parseInt(split[0]);
 
-    public static boolean isCardExpiryDated(String cardExpiryDate) throws ParseException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
+        return currentYear <= expiryYear && (currentMonth <= expiryMonth);
+    }
+
+    public static String getStrExpireYear(String cardExpiryDate){
+        String[] parts = cardExpiryDate.split("/");
+        if (parts.length == 2) {
+            return parts[0]+"/"+getCentury() + parts[1];
+        }else{
+            throw new PaymentException(PaymentError.CREDIT_CARD_EXPIRED);
+        }
+    }
+
+    public static String getCentury(){
         Calendar currentCalender = Calendar.getInstance();
-        Calendar expiryCalender = Calendar.getInstance();
-
-        Date expiryDate = dateFormat.parse(cardExpiryDate);
-        expiryCalender.setTime(expiryDate);
-
-        return currentCalender.after(expiryCalender);
-
+        var year = currentCalender.get(Calendar.YEAR);
+        return String.valueOf(year/100);
     }
 
 }
